38010e387a3f7302515c631471bc00fb97d55005
Takayuki Okazaki <takayuki.okazaki@gree.net>
2012-12-06 19:19:21 +0900

    Add minimum Solr PoC

--- src/main/java/org/watermint/sourcecolon/Main.java
+++ src/main/java/org/watermint/sourcecolon/Main.java
@@ -4,6 +4,8 @@ import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.beans.Field;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.core.CoreContainer;
 
@@ -16,39 +18,36 @@ import java.util.List;
  *
  */
 public class Main {
-    public static class SimpleItem {
-        @Field
-        String id;
-
-        @Field
-        String name;
-    }
-
     public static void main(String... args) throws Exception {
         File home = new File("./src/main/resources");
         File configFile = new File(home, "solr.xml");
         CoreContainer container = new CoreContainer(home.getAbsolutePath(), configFile);
         EmbeddedSolrServer server = new EmbeddedSolrServer(container, "sourcecolon");
+        try {
+            List<SolrInputDocument> docs = new ArrayList<>();
+            for (int i = 0; i < 100; i++) {
+                SolrInputDocument d = new SolrInputDocument();
+                d.setField("id", "data" + i);
+                d.setField("name", "name" + i);
+                docs.add(d);
+            }
+            server.add(docs);
 
-        SimpleItem i = new SimpleItem();
-        i.id = "d1";
-        i.name = "hoge";
+            server.commit();
 
-        server.addBean(i);
-        server.commit();
+            SolrQuery q = new SolrQuery();
+            q.setQuery("name:name3*");
 
-        SolrQuery q = new SolrQuery();
-        q.setQuery("*");
+            QueryResponse r = server.query(q);
 
-        QueryResponse r = server.query(q);
+            SolrDocumentList results = r.getResults();
+            results.setStart(10);
 
-        List<SimpleItem> results = r.getBeans(SimpleItem.class);
-
-        for (SimpleItem si : results) {
-            System.out.println("id : " + si.id);
-            System.out.println("name : " + si.name);
+            for (SolrDocument d : results) {
+                System.out.println(d.toString());
+            }
+        } finally {
+            server.shutdown();
         }
-
-        server.shutdown();
     }
 }
