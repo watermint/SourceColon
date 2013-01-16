91abe92036076d17d648aa80202e92f90c15866d
Takayuki Okazaki <takayuki.okazaki@gree.net>
2013-01-16 19:59:36 +0900

    Reorganize containers

--- src/main/webapp/js/sourcecolon.js
+++ src/main/webapp/js/sourcecolon.js
@@ -1,89 +1,87 @@
-
 var SourceColon = {
-  chunkSize: 100,
-  chunkProfile: function (page, index, chunks) {
-    var chunkIndex = Math.floor(index / SourceColon.chunkSize);
-    var chunkBegin = Math.max(chunkIndex * SourceColon.chunkSize + 1, page.lwm);
-    var chunkEnd = Math.min((chunkIndex + 1) * SourceColon.chunkSize, page.hwm);
-    var nextChunkBegin = (chunkIndex + 1) * SourceColon.chunkSize + 1;
-    var prevChunkEnd = chunkIndex * SourceColon.chunkSize - 1;
-    var nextChunk = null;
-    var prevChunk = null;
-
-    if (chunks.next && nextChunkBegin <= page.hwm) {
-      nextChunk = SourceColon.chunkProfile(page, nextChunkBegin, {next: true});
+  chunkSize: 1000,
+  lineHeight: 20,
+  chunkProfiles: function (page) {
+    var list = [];
+    var pageNum = Math.ceil((page.hwm - page.lwm + 1.0) / SourceColon.chunkSize);
+    for (var i = 0; i < pageNum; i++) {
+      var begin = i * SourceColon.chunkSize + page.lwm;
+      list.push({
+        page: page,
+        chunkBegin: begin,
+        chunkEnd: Math.min(begin + SourceColon.chunkSize - 1, page.hwm)
+      })
     }
-    if (chunks.prev && page.lwm < prevChunkEnd) {
-      prevChunk = SourceColon.chunkProfile(page, prevChunkEnd, {prev: true});
-    }
-
-    return {
-      page: page,
-      offset: chunks.offset,
-      target: null,
-      chunk: {
-        begin: chunkBegin,
-        end: chunkEnd
-      },
-      next: nextChunk,
-      prev: prevChunk
-    }
+    return list;
   },
-  newChunk: function (chunk) {
-    var c = $('<div>Loading.. ' + chunk.chunk.begin + ' - ' + chunk.chunk.end + '</div>');
-    c.attr('id', chunk.fileHash + '-' + chunk.chunk.begin);
-    c.appear(function () {
-      setTimeout(SourceColon.loadChunk(jQuery.extend({container: c}, chunk)), 10);
-    });
+  appendChunks: function (container, page, focusIndex) {
+    var chunks = [];
+    jQuery.each(SourceColon.chunkProfiles(page), function () {
+      var c = SourceColon.createChunk(this);
+      container.append(c);
+      chunks.push(jQuery.extend({
+        container: c}, this));
+    })
+    window.scrollTo(window.scrollX, focusIndex * SourceColon.lineHeight + container.position().top);
+    jQuery.each(chunks, function (i, d) {
+      $(this.container).appear(function () {
+        setTimeout(function() {
+          SourceColon.loadChunk(d);
+        }, 10);
+      })
+    })
+  },
+  createChunk: function (chunkProfile) {
+    var chunk = $('<div></div>');
+    var placeHolder = $('<div><h1>Place Holder for ' + chunkProfile.chunkBegin + '</h1></div>');
+    var placeHolderHeight = SourceColon.lineHeight * (chunkProfile.chunkEnd - chunkProfile.chunkBegin);
+    placeHolder.css('height', placeHolderHeight + 'px');
 
-    return c;
+    chunk.attr('id', chunkProfile.page.fileHash + "-" + chunk.chunkBegin);
+    chunk.append(placeHolder);
+
+    return chunk;
   },
   loadChunk: function (chunk) {
     jQuery.ajax('chunk.json', {
       data: {
-        fileHash: chunk.page.fileHash,
-        begin: chunk.chunk.begin,
-        end: chunk.chunk.end
+        begin: chunk.chunkBegin,
+        end: chunk.chunkEnd,
+        fileHash: chunk.page.fileHash
       },
       dataType: 'json',
       success: function (rows) {
         chunk.container.empty();
+
+        // dummy data
+        rows.rows = []
+        for (var i = 0; i < SourceColon.chunkSize; i++) {
+          rows.rows.push({
+            n: chunk.chunkBegin + i, t: 'Text line number #' + (chunk.chunkBegin + i)
+          })
+        }
+
         jQuery.each(rows.rows, function (i, d) {
-          var n = chunk.chunk.begin + i;
-          var t = "Text line number #" + n;
-          var row = $("<div id='" + chunk.page.fileHash + "-" + n + "'><span class='label label-info'>" + n + "</span> " + t + "</div>");
+          var n = d.n;
+          var t = d.t;
+          var lineNumber = "<span class='label'><a name='" + n + "'></a>" + n + "</span> ";
+          var chunkId = chunk.page.fileHash + "-" + n;
+          var row = $("<div id='" + chunkId + "'>" + lineNumber + t + "</div>");
 
           chunk.container.append(row);
         });
-
-        if (chunk.prev || chunk.next) {
-          setTimeout(function () {
-            if (chunk.prev) {
-              var prev = SourceColon.newChunk(chunk.prev);
-              chunk.container.before(prev);
-              window.scrollTo(window.scrollX, window.scrollY + chunk.container.height());
-            }
-            if (chunk.next) {
-              chunk.container.after(SourceColon.newChunk(chunk.next));
-            }
-          }, 100);
-        }
       }
     })
   },
-  loadCode: function (container, fileHash, index) {
+  loadPage: function(container, fileHash, focusIndex) {
     jQuery.ajax('page.json', {
+      data: {
+        fileHash: fileHash
+      },
       dataType: 'json',
-      success: function (page) {
-        var p = SourceColon.chunkProfile(page, index, {next: true, prev: true, offset: index});
-        var c = SourceColon.newChunk(p)
-        $(container).append(c);
+      success: function(page) {
+        SourceColon.appendChunks(container, page, focusIndex);
       }
     })
   }
 }
-
-$(document).ready(function () {
-  SourceColon.loadCode($('#source1234'), '1234', 501);
-})
-
