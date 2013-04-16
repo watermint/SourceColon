10ac36a326f9887b11ec51a16e275c967284eb2e
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2013-04-16 09:10:51 +0900

    fix for test

--- app/SourceColon.scala
+++ app/SourceColon.scala
@@ -7,8 +7,6 @@ import play.api._
 object SourceColon extends GlobalSettings {
   override def onStart(app: Application) {
     super.onStart(app)
-
-    Engine.startup()
   }
 
   override def onStop(app: Application) {
